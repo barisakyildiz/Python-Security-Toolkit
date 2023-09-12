d2967ea0e0687b6214d6cf479da9d478549b1100
Barış Akyıldız <barisakyildiz3@gmail.com>
2023-09-12 03:08:33 +0300

    tcp server added

---
+++ TCPServerExample.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+bind_ip = "0.0.0.0"
+bind_port = 9999
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+server.bind((bind_ip, bind_port))
+
+server.listen(5)
+
+print("[*] Listening on {}:{}".format(bind_ip, bind_port))
+
+#client-handling thread
+def handlingClient(client_socket):
+    request = client_socket.recv(1024)
+    print("[*] Recieved: {}".format(request))
+
+    #send back a response
+    client_socket.send("ACKNOWLEDGE".encode())
+    client_socket.close()
+
+while True:
+    client, addr = server.accept()
+    print("[*] Incoming Connection From: {}:{}".format(addr[0], addr[1]))
+
+    #client thread
+    client_handler = threading.Thread(target=handlingClient, args=(client,))
+    client_handler.start()
